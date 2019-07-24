b9fffef104f1c02f1b7534b653dfcc808c194d78
Andre <andrejp09@gmail.com>
2019-07-23 23:33:07 -0300

    ajuste mapeamento de classe

--- src/main/java/com/andrebongiolo/lavanderia/models/OrdemServico.java
+++ src/main/java/com/andrebongiolo/lavanderia/models/OrdemServico.java
@@ -1,13 +1,19 @@
 package com.andrebongiolo.lavanderia.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 
 @Entity
 @Table(name = "ORDEM_SERVICOS")
 @SequenceGenerator(name = "SEQ_ORDEM_SERVICOS", allocationSize = 10, sequenceName = "SEQ_ORDEM_SERVICOS")
+@JsonIgnoreProperties( ignoreUnknown = true)
 public class OrdemServico implements Entidade {
 
 	@Id
@@ -28,12 +34,21 @@ public class OrdemServico implements Entidade {
 
 	@Temporal(TemporalType.TIMESTAMP)
 	@Column(name = "DT_RETIRADA")
-	private Date dataRetirada;
+	private Date dataSaida;
 
-	@OneToMany(fetch = FetchType.EAGER)
-	@JoinColumn(name = "ID_ITEM")
-	private List<Item> items;
+	@Column(name = "VL_TOTAL", precision = 15, scale = 5)
+	private BigDecimal valorTotal;
 
+
+	@ManyToMany(cascade = {CascadeType.DETACH}, fetch = FetchType.EAGER)
+	@JoinTable(name = "ITENS_ORDEM",
+			joinColumns = {
+					@JoinColumn(name = "ID_ORDEM", referencedColumnName = "ID_ORDEM_SERVICO")
+			},
+			inverseJoinColumns = {
+					@JoinColumn(name = "ID_ITEM", referencedColumnName = "ID_ITEM")
+			})
+	private List<Item> itens = new ArrayList<>();
 
 	@Override
 	public Long getId() {
@@ -60,19 +75,29 @@ public class OrdemServico implements Entidade {
 		this.dataEntrada = dataEntrada;
 	}
 
-	public Date getDataRetirada() {
-		return dataRetirada;
+	public Date getDataSaida() {
+		return dataSaida;
 	}
 
-	public void setDataRetirada(Date dataRetirada) {
-		this.dataRetirada = dataRetirada;
+	public void setDataSaida(Date dataSaida) {
+		this.dataSaida = dataSaida;
 	}
 
-	public List<Item> getItems() {
-		return items;
+	public List<Item> getItens() {
+		return itens;
 	}
 
-	public void setItems(List<Item> items) {
-		this.items = items;
+	public void setItens(List<Item> itens) {
+		this.itens = itens;
 	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+
 }
